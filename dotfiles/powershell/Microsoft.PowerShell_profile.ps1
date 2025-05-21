3e68b807100fc806ec145afdbe82dd74e2f47168
balnc9 <matthewlchin1@gmail.com>
2025-05-21 04:39:51 -0400

    updated pshell+winfetch config

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Oh My Posh Unicorn theme
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\unicorn.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kali.omp.json" | Invoke-Expression
 
 # Run Winfetch
-& "C:\Tools\winfetch\winfetch.ps1"
+& "C:\Users\matth\Tools\winfetch\winfetch.ps1"
